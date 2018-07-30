7965b112dbe750639db0a403ddbc36b1f922eacc
ggbandit <ggbandit@hotmail.com>
2018-07-30 19:30:22 +0700

    login page

---
+++ register/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register Page</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <script src="../js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+        <div class="box">
+            <h2>Login</h2>
+            <form >
+                <div class="inputBox">
+                    <input type="text" name="" required="">
+                    <label>Username</label>
+                </div>
+                <div class="inputBox">
+                    <input type="password" name="" required="">
+                    <label>Password</label>
+                </div>
+                <div>
+                    <input type="submit" name="" value="Submit">
+                </div>
+                <div class="mt-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Sign up</a></li>
+                        <li class="breadcrumb-item"><a href="#">Forget password</a></li>
+                    </ol>
+                </nav>
+                </div>
+            </form>
+        </div>
+</body>
+</html>
